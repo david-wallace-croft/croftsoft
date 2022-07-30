17bfb4e21030c1ca8d6e7d8b97b8989abc8fbb8e
David Wallace Croft <david@CroftSoft.com>
2022-07-30 17:15:22 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# croftsoft
+
+- Placeholder for crates.io
+- http://www.croftsoft.com/
+- Published to crates.io on 2022-07-30
---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+// =============================================================================
+//! - Placeholder for crates.io
+//! - <http://www.croftsoft.com/>
+//! - Rust version: 2022-07-30
+//! - Rust since: 2022-07-30
+// =============================================================================
+
+pub fn print_copyright() {
+  println!("Copyright 2022 CroftSoft Inc");
+}
