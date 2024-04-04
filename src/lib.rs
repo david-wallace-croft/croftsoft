73db46fa617c7923a5e87da3ca2cffb2101e5d91
David Wallace Croft <david@CroftSoft.com>
2024-04-04 07:18:17 -0500

    version 0.10.1

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,15 @@
 //! - Re-export of the com-croftsoft-* crates
 //! - <https://www.CroftSoft.com/>
 //! - Created: 2022-07-30
-//! - Updated: 2023-03-21
+//! - Updated: 2024-04-04
 // =============================================================================
 
 pub mod core {
   #[doc(inline)]
   pub use com_croftsoft_core::*;
+}
+
+pub mod lib {
+  #[doc(inline)]
+  pub use com_croftsoft_lib::*;
 }
